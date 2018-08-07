0a6daf92ae9e02702c82ba95fbe5a1498703d1f1
mlaakso@dev <laakso.mavrick@gmail.com>
2018-08-06 21:15:57 -0400

    markdown support server side

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'virtus'
 gem 'bootstrap', '~> 4.1.3'
 # jQuery
 gem 'jquery-rails'
+# Markdown support
+gem 'redcarpet'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,20 @@
 module ApplicationHelper
+  include Redcarpet
 
+  # Provides the "original" tags for use in the header
   def nav_tags
     Tag.where(original: true)
   end
 
+  # For checking errors in @object.errors?
   def has_error(obj, key)
     obj.errors.details[key].empty? == false
+  end
+
+  # Render markdown ¯\_(ツ)_/¯
+  def render_markdown(to_render)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown.render(to_render)
   end
 
 end
