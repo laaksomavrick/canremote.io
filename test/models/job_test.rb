78d5357e6d8b2aada0f546f458e6819ddb0dc7c3
Mavrick Laakso <laakso.mavrick@gmail.com>
2018-08-16 09:18:13 -0400

    Update job_test.rb

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class JobTest < ActiveSupport::TestCase
 
   test "should only retrieve active job postings" do
-    not_expired = Job.not_expired.pluck(:created_at).select { |ca| ca > 31.days.ago }
-    assert not_expired.length > 0
+    expired = Job.not_expired.pluck(:created_at).select { |ca| ca <= 31.days.ago }
+    assert expired.length == 0
   end
 
 end
